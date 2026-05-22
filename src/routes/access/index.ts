368ac64935268878398f8a188bea3bd8596e476f
ololobina-blip <ololobina@gmail.com>
2026-05-22 21:04:17 +0500

    Delete src directory

--- src/routes/access/index.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import get from './get'
-import post from './post'
-
-export default async function init(instance: FastifyInstance) {
-  await instance.register(get)
-  await instance.register(post)
-}
--- src/routes/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import login from './login'
-
-export default async function init(instance: FastifyInstance) {
-  await instance.register(login, { prefix: '/login' })
-}
